00490365150ad997bdc4dfe4333d2e07b4064194
David Debreczeni <david@octarine-labs.com>
2014-04-08 14:07:30 +0200

    add option to overwrite base_uri and clean up user's info hash

--- lib/omniauth/strategies/terracycle.rb
+++ lib/omniauth/strategies/terracycle.rb
@@ -6,44 +6,32 @@ module OmniAuth
     class TerraCycle < OmniAuth::Strategies::OAuth2
       option :name, 'terracycle'
       option :country_code, :us
+      option :base_uri, nil
 
       def initialize(*args, &block)
         super(*args, &block)
 
         country_code = options.country_code
         domain = OmniAuth::TerraCycle::Country.domain_for_code(country_code)
-        base_uri = "https://api.terracycle.#{domain}"
+        base_uri = options.base_uri || "https://api.terracycle.#{domain}"
 
         options.client_options = {
-          site:          "#{base_uri}/v2",
+          site:          base_uri,
           authorize_url: "#{base_uri}/oauth/authorize",
           token_url:     "#{base_uri}/oauth/token"
         }
       end
 
-      uid { raw_info['uuid'] }
+      uid do
+        account['uuid']
+      end
 
       info do
-        profile = raw_info['profile']
-        first_name = profile['first_name']
-        last_name = profile['last_name']
-        name = "#{first_name} #{last_name}"
-
-        {
-          name: name,
-          first_name: first_name,
-          last_name: last_name,
-          email: raw_info['email'],
-          profile: profile
-        }
+        account
       end
 
-      extra do
-        { 'raw_info' => raw_info }
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get('/account').parsed['account']
+      def account
+        @account ||= access_token.get('/v2/account').parsed['account']
       end
     end
   end
